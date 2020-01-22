1f388c3daecbf5b5d1aa087eac5e170ee8eb0901
laurent <htd.laurent@gmail.com>
2020-01-22 17:58:45 +0100

    isolate debug

--- src/config/data.js
+++ src/config/data.js
@@ -1,3 +1,27 @@
+/*
+ * Copyright 2020 SpinalCom - www.spinalcom.com
+ * 
+ * This file is part of SpinalCore.
+ * 
+ * Please read all of the following terms and conditions
+ * of the Free Software license Agreement ("Agreement")
+ * carefully.
+ * 
+ * This Agreement is a legally binding contract between
+ * the Licensee (as defined below) and SpinalCom that
+ * sets forth the terms and conditions that govern your
+ * use of the Program. By installing and/or using the
+ * Program, you agree to abide by all the terms and
+ * conditions stated or referenced herein.
+ * 
+ * If you do not agree to abide by these terms and
+ * conditions, do not demonstrate your acceptance and do
+ * not install or use the Program.
+ * You should have received a copy of the license along
+ * with this file. If not, see
+ * <http://resources.spinalcom.com/licenses.pdf>.
+ */
+
 import geographicService from "spinal-env-viewer-context-geographic-service";
 import {
   SERVICE_NAME,
@@ -30,6 +54,16 @@ function getOrAddModelIfMissing(array, model) {
 
 function sortBIMObjectByModel(arrayOfBIMObject) {
   let arrayModel = [];
+  for (const key in spinal.BimObjectService.mappingNameByModel) {
+    if (spinal.BimObjectService.mappingNameByModel.hasOwnProperty(key)) {
+      const model = spinal.BimObjectService.mappingNameByModel[key];
+      arrayModel.push({
+        selection: [],
+        model
+      })
+    }
+  }
+
   for (const nodeBIMObject of arrayOfBIMObject) {
     const bimFileId = nodeBIMObject.info.bimFileId.get();
     const dbId = nodeBIMObject.info.dbid.get();
